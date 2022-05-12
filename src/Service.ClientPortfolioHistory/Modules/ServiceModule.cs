92564586be752db643364dcbc69e196544e3a8fa
Maxim Gorin <maksim.g@smplt.net>
2022-05-12 13:00:08 +0300

    Fix after nosql update

--- src/Service.ClientPortfolioHistory/Modules/ServiceModule.cs
+++ src/Service.ClientPortfolioHistory/Modules/ServiceModule.cs
@@ -17,7 +17,7 @@ namespace Service.ClientPortfolioHistory.Modules
     {
         protected override void Load(ContainerBuilder builder)
         {
-            var myNoSqlClient = builder.CreateNoSqlClient(Program.ReloadedSettings(e => e.MyNoSqlReaderHostPort));
+            var myNoSqlClient = builder.CreateNoSqlClient(Program.Settings.MyNoSqlReaderHostPort, Program.LogFactory);
 
             var factory = new MyGrpcClientFactory(Program.Settings.CandlesServiceGrpcUrl);
             
